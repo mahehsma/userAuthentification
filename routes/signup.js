f2b4282a3bffad218707523f5ed2df55c58ebf17
mahehsma <maxi.hehl@gmail.com>
2021-02-21 09:42:07 +0100

    optimized salting

--- routes/signup.js
+++ routes/signup.js
@@ -1,32 +1,39 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../bin/db");
-var bcrypt = require('bcryptjs');
-var uuid = require('uuid')
-var userMiddleware = require('../middleware/users');
+var bcrypt = require("bcryptjs");
+var uuid = require("uuid");
+var userMiddleware = require("../middleware/users");
 
 router.get("/", (req, res, next) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("sign-up");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("sign-up");
 });
 
-router.post("/", userMiddleware.validateRegister, userMiddleware.alreadyExists, (req, res, next) => {
+router.post(
+  "/",
+  userMiddleware.validateRegister,
+  userMiddleware.alreadyExists,
+  (req, res, next) => {
     console.log(req.body.user);
+    bcrypt.genSalt((err, salt) => {
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) return res.status(500).send({ msg: err });
 
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err) return res.status(500).send({msg: err}); 
+        const sql = `INSERT INTO users(id, username, password, email, created,last_login) VALUES('${uuid.v4()}', ${db.escape(
+          req.body.username
+        )}, ${db.escape(hash)}, ${db.escape(req.body.email)}, now(), now());`;
 
-        const sql = `INSERT INTO users(id, username, password, email, created,last_login) VALUES('${uuid.v4()}', ${db.escape(req.body.username)}, ${db.escape(hash)}, ${db.escape(req.body.email)}, now(), now());`;
-       
-            db.query(sql, (err, result) => {
-        if(err){
+        db.query(sql, (err, result) => {
+          if (err) {
             console.log(err);
-            return res.status(400).send({msg: err});
-        }
-        return res.status(201).send({msg: "Registered!"});
-       });
-
+            return res.status(400).send({ msg: err });
+          }
+          return res.status(201).send({ msg: "Registered!" });
+        });
+      });
     });
-});
+  }
+);
 
 module.exports = router;
